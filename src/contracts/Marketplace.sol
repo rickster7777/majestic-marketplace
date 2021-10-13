b683f57d372d512967e762eaa2583d80ab7dd77c
rishav777 <kashyaprishav246@gmail.com>
2021-10-14 00:03:12 +0530

    first patt of majestic marketplace

---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,11 @@
+//pragma solidity ^0.5.0;
+pragma solidity >=0.4.20;
+
+contract Marketplace {
+    string public name;
+
+    constructor() public {
+        name = "Dapp University Marketplace";
+    }
+
+}
